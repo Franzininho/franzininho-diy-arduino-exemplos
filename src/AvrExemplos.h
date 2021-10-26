752cc2ff2ea82fabd667f909c2d05e81a2a0fd1a
RodrigoDornelles <rodrigo@dornelles.me>
2021-10-26 20:54:01 -0300

    Transformar em biblioteca #2

---
+++ src/AvrExemplos.h
@@ -0,0 +1,8 @@
+/***************************************************************
+  Avr Exemplos
+  
+  Por favor manter este arquivo no projeto, 
+  o mesmo é necessário para ser considerado uma biblioteca válida.
+
+  Autor: Rodrigo Dornelles
+***************************************************************/
